912cb0ee3c2459abe3868ff57db0ae25b932e5cf
wtz <wtz@sh.com>
2025-12-12 15:54:23 +0800

    chore: release v0.5.3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
+import { createApp } from "vue";
+import { registerSW } from "virtual:pwa-register";
+import App from "./App.vue";
+import "./style.css";
 
-createApp(App).mount('#app')
+registerSW({ immediate: true });
+
+createApp(App).mount("#app");
